ad88d4e2e43a3612d2aaaff9c767890bd411dfd3
lipskij <emil.lipskij@gmail.com>
2020-10-16 18:37:38 +0300

    refactoring

--- script.js
+++ script.js
@@ -1,17 +1,16 @@
-const ball = document.querySelector(".circle");
-const txt = document.querySelector("p");
-const prevAns = document.querySelector(".prev-ans");
+const ball = document.querySelector('.circle');
+const txt = document.querySelector('p');
+const prevAns = document.querySelector('.prev-ans');
 
-// TODO: add animation
-
-ball.addEventListener("click", () => {
-  const num = document.createElement("div");
+ball.addEventListener('click', () => {
+  const num = document.createElement('div');
   num.value = getRndInteger();
   ball.appendChild(txt);
-  if (num.value == 0) {
-    txt.innerHTML = "NO";
+
+  if (num.value === 0) {
+    txt.innerHTML = 'NO';
   } else {
-    txt.innerHTML = "YES";
+    txt.innerHTML = 'YES';
   }
   renderItem();
   limitAnswers();
@@ -23,12 +22,11 @@ function getRndInteger() {
 
 // render prev answers
 function renderItem() {
-  prevAns.innerHTML += ` ${txt.innerHTML}.`;
+  return (prevAns.innerHTML += ` ${txt.innerHTML}.`);
 }
 // limit answers to only 3
 function limitAnswers() {
-  let answers = prevAns.innerHTML;
-  if (answers.length >= 32) {
-    prevAns.innerHTML = `Previous answer: ${txt.innerHTML}.`;
+  if (prevAns.innerHTML.length >= 32) {
+    return (prevAns.innerHTML = `Previous answer: ${txt.innerHTML}.`);
   }
 }
